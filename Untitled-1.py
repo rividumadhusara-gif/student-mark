cf726e093b49eb9e24b8024520bcdb6cb565f790
rividumadhusara-gif <rividumadhusara@gmail.com>
2026-07-17 15:07:26 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,10 @@
+student_no=int(input("Enter the your student number:"))
+mark=int(input("Enter the your marks:"))
+if mark > 80:
+    print("grade=","A")
+elif mark>70 :
+    print("grade=","B") 
+elif mark >60 :
+    print("grade=","C")
+else :
+    print("grade=","F")  
